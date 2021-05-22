948d230f3533ac239509ce05d9a8bd2173ba262d
AllSafeCybercurity <skull32273227@gmail.com>
2021-05-22 16:23:52 -0700

    Web in C CPP

---
+++ Source.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Contain type:text/html \n\n") ;
+
+    printf("<html>");
+    printf("<head>");
+    
+    printf("<title>");
+    printf("FuCk");
+    printf("</title>"); 
+
+    printf("</head>");
+
+    printf("<body>");
+    printf("Fuck the WOrld");
+    printf("C is Fast && Furious");
+    printf("</body>");
+
+    printf("<footer>");
+    printf("Made in C");
+    printf("</footer>");
+
+    printf("</html>");
+}
---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Containt type:text/html \n\n";
+    cout << "<html>";
+    cout << "<head><title>Hello</title></head>";
+    cout << "<body>";
+    cout << "<h1>Hello World</h1>";
+    cout << "<p>Here is  your website</p>";
+    cout << "</body></html>";
+    return 0;
+
+}
